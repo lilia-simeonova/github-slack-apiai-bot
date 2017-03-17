b92de86616694210ed86f0c282d943e63eafad13
lilia.simeonova <lilia.valentinova.simeonova@gmail.com>
2017-03-16 19:12:07 -0700

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
-// Module must be started with environment variables
-//
-//  accesskey="api.ai client access key"
-//  slackkey="slack bot key"
-//
-
-'use strict';
-
 const Botkit = require('botkit');
-
 const apiai = require('apiai');
 const uuid = require('node-uuid');
-
 const http = require('http');
 var axios = require('axios');
 var GitHub = require('github-api');
-
 const Entities = require('html-entities').XmlEntities;
 const decoder = new Entities();
-
 const apiAiAccessToken = process.env.accesstoken;
 const slackBotKey = process.env.slackkey;
-
-const github = new GitHub({ token: process.env.githubtoken});
-
-console.log(process.env.githubtoken);
+var github = new GitHub({ token: process.env.githubtoken});
 const devConfig = process.env.DEVELOPMENT_CONFIG == 'true';
-
 const npmKeyword = require('npm-keyword');
-
 const apiaiOptions = {};
 
 if (devConfig) {
@@ -36,9 +19,6 @@ if (devConfig) {
 }
 
 const apiAiService = apiai(apiAiAccessToken, apiaiOptions);
-
-
-
 const sessionIds = new Map();
 
 const controller = Botkit.slackbot({
@@ -46,47 +26,33 @@ const controller = Botkit.slackbot({
   //include "log: false" to disable logging
 });
 
-
-
 var bot = controller.spawn({
   token: slackBotKey
 }).startRTM();
-
 
 function isDefined(obj) {
   if (typeof obj == 'undefined') {
     return false;
   }
-
   if (!obj) {
     return false;
   }
-  
   return obj != null;
 }
 
 const TimeOutError = 'I couldn\'t resolve your request';
 const TimeoutDuration = 90000;
-
-function timeoutAfter(duration) {
-  return new Promise((_, reject) => setTimeout(() => reject(TimeOutError), duration));
-}
+const timeoutAfter = duration => new Promise((_, reject) => setTimeout(() => reject(TimeOutError), duration));
 
 function firstReady(...promises) {
   let completed = false;
-  const complete = f => {
-    return result => {
-      if (!completed) {
-        completed = true;
-        f(result);
-      }
-    };
+  const complete = f => result => {
+    if (!completed) {
+      completed = true;
+      f(result);
+    }
   };
-  return new Promise((resolve, reject) => {
-    promises.forEach(p => {
-      p.then(complete(resolve), complete(reject));
-    });
-  });
+  return new Promise((resolve, reject) => promises.forEach(p => p.then(complete(resolve), complete(reject))));
 }
 
 controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambient'], (bot, message) => {
@@ -108,12 +74,9 @@ controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambien
         if (requestText.indexOf(botId) > -1) {
           requestText = requestText.replace(botId, '');
         }
-
         if (!sessionIds.has(channel)) {
           sessionIds.set(channel, uuid.v1());
         }
-
-        console.log('Start request ', requestText);
         let request = apiAiService.textRequest(requestText,
           {
             sessionId: sessionIds.get(channel),
@@ -127,28 +90,28 @@ controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambien
                 }
             ]
           });
-
         request.on('response', (response) => {
-            
           if (isDefined(response.result)) {
             let responseText = response.result.fulfillment.speech;
             let responseData = response.result.fulfillment.data;
             let action = response.result.action;
             let parameters = response.result.parameters;
-            console.log('action is:', action);
-            
             if (action === 'search') {
               let searchProject = response.result.parameters['searchProject'];
               let searchUser = response.result.parameters['searchUser'];
+
+              // The action variable would be the value that we defined under every intent.
+              let action = response.result.action;
+
+              // The parameters variable will show us the value of the entities.
+              let parameters = response.result.parameters;
               let text = response.result.parameters['text'][0];
               var search = github.search();
               if (searchProject) {
                 console.log(searchProject, text)
                 firstReady(
                   search.forRepositories({ q: text, sort: 'stars', order: 'desc', page:1, per_page: 5}),
-                    timeoutAfter(TimeoutDuration)
-                  )
-                  .then(r => { 
+                  timeoutAfter(TimeoutDuration)).then(r => { 
                     if (r.data) {
                       console.log(r.data);
                       responseText = 'Here are the first 3 result:' + '\n' + r.data[0]['full_name'] + '\n' + r.data[1]['full_name'] + '\n' + r.data[2]['full_name'];
@@ -165,12 +128,7 @@ controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambien
                   });
               } else if (searchUser) {
                 console.log(text);
-                  firstReady(
-                    search.forUsers({ q: text}),
-                    timeoutAfter(TimeoutDuration)
-                  )
-                  .then(
-                    r => {
+                  firstReady(search.forUsers({ q: text}), timeoutAfter(TimeoutDuration)).then(r => {
                       let user = r.data[0]['html_url'];
                       console.log(user);
                       if (user) {
@@ -183,38 +141,28 @@ controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambien
                   })       
               }
             } else if(action === 'follow') {
-                var result = response.result.parameters['text'];
-                console.log('result is:',result);
-                if(result) {
-                    //console.log(result);
-                  var user = github.getUser(result);
-                    //console.log(user);   
-                  firstReady(
-                    user.follow(),
-                    timeoutAfter(TimeoutDuration)
-                  )
-                  .then( 
-                  r => { 
-                    console.log(r);
-                    if(r) {
-                      let responseText = 'You successfuly followed ' + result;
-                      bot.reply(message, responseText);
-                    } else {
-                      responseText = 'There was some problem. Please try again.';
-                      bot.reply(message, responseText);
-                    }  
-                  });
+              var result = response.result.parameters['text'];
+              console.log('result is:',result);
+              if(result) {
+                  //console.log(result);
+                var user = github.getUser(result);
+                  //console.log(user);   
+                firstReady(user.follow(), timeoutAfter(TimeoutDuration)).then(r => { 
+                  if(r) {
+                    let responseText = 'You successfuly followed ' + result;
+                    bot.reply(message, responseText);
+                  } else {
+                    responseText = 'There was some problem. Please try again.';
+                    bot.reply(message, responseText);
+                  }  
+                });
               }   
             } else if(action === 'createRepo') {
               var result = response.result.parameters['text'];
               if(result) {
                 console.log(result);
                 var user = github.getUser();
-                firstReady(
-                  user.createRepo({name: result}),
-                  timeoutAfter(TimeoutDuration)
-                )
-                .then( r => {
+                firstReady(user.createRepo({name: result}), timeoutAfter(TimeoutDuration)).then(r => {
                   console.log(r);
                   let responseText = "You successfuly created repo " + result;
                   bot.reply(message, responseText);
@@ -222,7 +170,7 @@ controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambien
               }  
             }
             if (isDefined(responseData) && isDefined(responseData.slack)) {
-              try{
+              try {
                 bot.reply(message, responseData.slack);
               } catch (err) {
                 bot.reply(message, err.message);
@@ -236,19 +184,16 @@ controller.hears(['.*'], ['direct_message', 'direct_mention', 'mention', 'ambien
             }
             }
         });
-
         request.on('error', (error) => console.error(error));
         request.end();
       }
     }
   } catch (err) {
-      console.error(err);
+    console.error(err);
   }
 });
 
-
 //Create a server to prevent Heroku kills the bot
 const server = http.createServer((req, res) => res.end());
-
 //Lets start our server
 server.listen((process.env.PORT || 5000), () => console.log("Server listening"));
